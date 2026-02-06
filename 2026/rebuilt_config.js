084fba923a7c392ba7f00a1fd79a428ffd712bc1
kw2194 <128317434+kw2194@users.noreply.github.com>
2026-02-06 08:45:52 -0600

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026wiapp",
+      "defaultValue": "2025wila",
       "required": "true"
     },
     { "name": "Match Level",
